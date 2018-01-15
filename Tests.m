acb753af3e6275d6567c7007977a5ac795807ad0
kfli <kristoffer.lindvall@gmail.com>
2018-01-15 11:34:12 +0100

    Update Tests.m

--- Tests.m
+++ Tests.m
@@ -11,13 +11,11 @@ fun1D = @(x)(x-2*cos(x));
 x0 = 2;
 
 tic;y1D = fsolve(fun1D,x0);toc
-disp('fsolve solution')
-disp(y1D)
+disp(['fsolve solution: ', num2str(y1D)])
 
 phi1D = @(x)(2*cos(x));
-tic;U1D = SIR(phi1D,x0,0,0.95);toc
-disp('SIR solution')
-disp(U1D)
+tic;U1D = SIR(phi1D,x0,0);toc
+disp(['SIR solution: ', num2str(U1D)])
 
 %% 2D function - f1(x1,x2)=x1-cos(x2),
 %                f2(x1,x2)=x2-3cos(x1).
@@ -31,41 +29,45 @@ fun2D = @(x)[x(1)-cos(x(2)),...
 x0 = [-2,-2];
 
 tic;y2D = fsolve(fun2D,x0);toc
-disp('fsolve solution')
-disp(y2D)
+disp(['fsolve solution: ', num2str(y2D)])
 
 phi2D = @(x)[cos(x(2)),3*cos(x(1))];
-
 tic;U2D = SIR(phi2D,x0,1);toc
-disp('SIR solution')
-disp(U2D)
+disp(['SIR solution: ', num2str(U2D)])
+
 
 %% Convergence diagrams
-convdiag = zeros(41,41);
+convdiag = zeros(51,51);
 k = 0;
-for i = -5:0.25:5
+for i = -5:0.2:5
     k = k+1;
     l = 0;
-    for j = -5:0.25:5
+    for j = -5:0.2:5
         l = l+1;
         x0 = [i,j];
-        [U2D,iter] = SIR(phi2D,x0,1,0.9999);
+        [U2D,iter]  = SIR(phi2D,x0,1);
+        if iter > 100
+            iter = 150;
+        end
         convdiag(k,l)=iter;
     end
 end
 
 %%     Surfplot    %%
-[X,Y] = meshgrid(-5:0.25:5);
+[X,Y] = meshgrid(-5:0.2:5);
 f1 = X-cos(Y); f2 = Y-3*cos(X);
 Z = (f1.*f1+f2.*f2)/2;
 
 figure(1)
 surf(X,Y,Z,convdiag)
-view([-40 60])
+view([-40 50])
 title('Convergence diagram: SIR-s [f_1 = x_1-cos(x_2), f_2 = x_2-3*cos(x_1)]')
 xlabel('$$\textbf{x}_1$$','Interpreter', 'Latex')
 ylabel('$$\textbf{x}_2$$','Interpreter', 'Latex')
-zlabel('$$\textbf{f}{\cdot}\textbf{f/2}$$','Interpreter', 'Latex')
-hcb=colorbar;
-title(hcb,'iterations')
+zlabel('$$\textbf{f}{\cdot}\textbf{f}/2$$','Interpreter', 'Latex')
+caxis([0 150])
+hcb = colorbar;
+hcb.Ticks = [0,50,100,150];
+hcb.TickLabels = {'0 it.','50 it.','100 it.','No conv'};  
 colormap(jet)
+
